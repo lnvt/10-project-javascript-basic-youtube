1a0c85727999e4eb83b448c61eda3fde31f7b85f
DESKTOP-7COTCUM\lengu <lenguyenvietthang1908@gmail.com>
2021-01-17 20:21:04 +0700

    10 project JS basic

---
+++ auto_write_text/script.js
@@ -0,0 +1,16 @@
+const text = "Learn 10 project in Ytb";
+
+let index = 0;
+
+function writeText(){
+    document.body.innerText = text.slice(0, index);
+
+    index++;
+
+    if(index > text.length) {
+        index = 0;
+
+    }
+}
+
+setInterval(writeText, 100);
---
+++ background-changer/script.js
@@ -0,0 +1,12 @@
+const btn = document.getElementById("btn");
+
+btn.addEventListener('click',() => {
+    document.body.style.background = randomBg();
+    console.log(randomBg());
+});
+
+function randomBg(){
+    return `hsl( ${Math.floor(Math.random() * 360)},100%, 50%)`;
+}
+
+console.log(`abc`);
